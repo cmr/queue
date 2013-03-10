16984f86206a66b8706f192fc9d604b578f61422
Corey Richardson <corey@octayn.net>
2013-03-10 14:29:21 -0400

    w1

--- webservice.lua
+++ webservice.lua
@@ -1,4 +1,9 @@
+local config = require "config"
+local luasql = require "luasql.postgres"
 local json = require "cjson"
+
+local env = luasql.postgres()
+local conn = env:connect(config.db.name, config.db.user, config.db.pass, config.db.host, config.db.port)
 
 function run(wsapi_env)
 	local success, table = pcall(function() json.decode(wsapi_env.input:read()) end)
@@ -8,6 +13,8 @@ function run(wsapi_env)
 		local command = table.command
 		local person = table.person
 		local item = table.item
+		local query = [[ WITH uid AS (SELECT userid FROM users WHERE username = '%s')
+		INSERT INTO items (itemid, content, sorted, user) VALUES ('%s', '%s', FALSE, uid); ]]
 		print(("%s %s %s"):format(command, person, item))
 		local function suc()
 			coroutine.yield([[{"success": true}]])
