a64112000af7359570c03d9a8da30b3c4c508b21
Corey Richardson <corey@octayn.net>
2013-04-02 21:54:58 -0400

    Support dequeueing; use tweed

--- webservice.lua
+++ webservice.lua
@@ -1,40 +1,69 @@
-module(..., package.seeall)
-
 local config = require "config"
 local luasql = require "luasql.postgres"
-local json = require "cjson"
 local uuid = require "uuid"
+local tweed = require "tweed"
+local inspect = require "inspect"
+local cjson = require "cjson"
 
 local env = assert(luasql.postgres())
-local conn = assert(env:connect(config.db.name, config.db.user, config.db.pass, config.db.host, config.db.port))
 
-local function error_()
-	return 400, {["Content-Type"] = "application/json"}, coroutine.wrap(function()
-		coroutine.yield([[{"success": false}]])
-	end)
-end
+local site = tweed.make_site {
+	[tweed.string 'username'] = {
+		[tweed.POST] = function(context)
+			-- enqueue item
+			local item = context.request.body
+			local conn = assert(env:connect(config.db.name, config.db.user, config.db.pass, config.db.host, config.db.port))
+			local userid = conn:execute(("SELECT userid FROM users WHERE username = '%s'"):format(conn:escape(context.params.username))):fetch()
+			if not userid then
+				context.response:err(404, "no such user")
+				return
+			end
 
-function run(wsapi_env)
-	local req = wsapi_env.input:read()
-	print(req)
-	local success, table = pcall(function() return json.decode(req) end)
-	if not success then
-		return error_()
-	else
-		local command = table.command
-		local account = table.account
-		local item = table.item
-		if command == "enqueue" then
-			local query = [[ INSERT INTO items (itemid, content, sorted, userid) VALUES ('%s', '%s', FALSE,
-			(SELECT userid FROM users WHERE username = '%s')); ]]
-			assert(conn:execute(query:format(uuid.new(), conn:escape(item), conn:escape(account))))
+			print (item)
+			local query = [[ INSERT INTO items (itemid, content, sorted, userid) VALUES ('%s', '%s', FALSE, '%s'); ]]
+			query = query:format(uuid.new(), conn:escape(item), conn:escape(userid))
+			assert(conn:execute(query))
 			conn:commit()
-		else
-			return error_()
-		end
+		end,
+		[tweed.GET] = function(context)
+			local itemid = context.request.body
+			local conn = assert(env:connect(config.db.name, config.db.user, config.db.pass, config.db.host, config.db.port))
+			local userid = conn:execute(("SELECT userid FROM users WHERE username = '%s'"):format(conn:escape(context.params.username))):fetch()
+			if not userid then
+				context.response:err(404, "no such user")
+				return
+			end
 
-		return 200, {["Content-Type"] = "application/json"}, coroutine.wrap(function()
-			coroutine.yield([[{"success": true}]])
-		end)
-	end
+			local query = [[ SELECT itemid, content, sort_index FROM items WHERE userid = '%s'; ]]
+			local cursor = conn:execute(query:format(userid))
+			local results = {}
+			for i = 1, cursor:numrows() do
+				local id, content, idx = cursor:fetch()
+				results[i] = {id=id, content=content, idx=idx or -1}
+			end
+
+			context.response:json(cjson.encode(results))
+		end,
+		[tweed.DELETE] = function(context)
+			local id = context.request.body
+			local conn = assert(env:connect(config.db.name, config.db.user, config.db.pass, config.db.host, config.db.port))
+			local userid = conn:execute(("SELECT userid FROM users WHERE username = '%s'"):format(conn:escape(context.params.username))):fetch()
+			if not userid then
+				context.response:err(404, "no such user")
+				return
+			end
+
+			local query = [[ DELETE FROM items WHERE itemid = '%s'; ]]
+			local cursor = conn:execute(query:format(id))
+
+			context.response:json(cjson.encode({success=true}))
+		end,
+	}
+}
+
+site.error_handlers[400] = function(site, ...)
+	local res = context.response
+	res:json(cjson.encode({success=false, err={...}}))
 end
+
+return site
