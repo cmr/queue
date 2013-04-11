b60f76266d015b36a31ab769398dfac54033bbe1
Corey Richardson <corey@octayn.net>
2013-04-10 22:30:35 -0400

    Refactor API access out to its own module
    And do good error handling. And remove pointless concurrency.

---
+++ ircbot/api.lua
@@ -0,0 +1,67 @@
+local config = require "config"
+local inspect = require "inspect"
+local ltn12 = require "ltn12"
+local http = require "socket.http"
+local cjson = require "cjson"
+
+local M = {}
+
+function M.push(person, item, tags)
+	local tab = {}
+	print ("making request to " .. config.api_url .. person)
+	local _, status = http.request{
+		url = config.api_url .. person, 
+		method = "POST",
+		headers = {["Content-Length"]=#item},
+		source = ltn12.source.string(item),
+		sink = ltn12.sink.table(tab)
+	}
+
+	print ("got " .. table.concat(tab))
+	return status == 200, cjson.decode(table.concat(tab))
+end
+
+function M.getall(person)
+	local tab = {}
+	local _, status = http.request{
+		url = config.api_url .. person, 
+		method = "GET",
+		sink = ltn12.sink.table(tab)
+	}
+
+	return status == 200, cjson.decode(table.concat(tab))
+end
+
+function M.pop(person)
+	local suc, things = M.getall(person)
+	print(inspect(things))
+	if not suc then return things end
+	local item = things[1]
+	pcall(M.delete(person, item.id))
+
+	return true, item.content
+end
+
+function M.getbytag(person, tag)
+	local tab = {}
+	local _, status = http.request{
+		url = config.api_url .. person .. '?tag=' .. tag, 
+		method = "GET",
+		sink = ltn12.sink.table(tab)
+	}
+
+	return status == 200, cjson.decode(table.concat(tab))
+end
+
+function M.delete(person, id)
+	local tab = {}
+	local _, status = http.request{
+		url = config.api_url .. person .. '?id=' .. id, 
+		method = "DELETE",
+		sink = ltn12.sink.table(tab)
+	}
+
+	return status == 200, cjson.decode(table.concat(tab))
+end
+
+return M
--- ircbot/queuebot.lua
+++ ircbot/queuebot.lua
@@ -1,58 +1,13 @@
 require "luarocks.loader"
 require "irc"
+
+local api = require "api"
+
 local config = require "config"
 local inspect = require "inspect"
-local lanes = require "lanes".configure()
-local linda = lanes.linda()
 
 local re = require "re"
 local sleep = require "socket".sleep
-local id = 0
-local queued_requests = {}
-
-local send_request = lanes.gen("*", function(command, account, item, id)
-	local http = require "socket.http"
-	local ltn12 = require "ltn12"
-	local cjson = require "cjson"
-
-	local tab = {}
-
-	if command == "push" then
-		local _, status = http.request{
-			url = config.api_url .. account, 
-			method = "POST",
-			headers = {["Content-Length"]=#item},
-			source = ltn12.source.string(item),
-			sink = ltn12.sink.table(tab)
-		}
-		if status ~= 200 then
-			linda:set(id, false)
-			print("http req failed: " .. tostring(status) .. tostring(table.concat(tab)))
-		else
-			linda:set(id, true)
-		end
-	elseif command == "pop" then
-		local _, status = http.request{
-			url = config.api_url .. account, 
-			method = "GET",
-			sink = ltn12.sink.table(tab)
-		}
-		if status ~= 200 then
-			linda:set(id, false)
-			print("http req failed: " .. tostring(status) .. tostring(table.concat(tab)))
-		else
-			local obj = cjson.decode(table.concat(tab))[1]
-			linda:set(id, obj.content)
-			local _, status = http.request{
-				url = config.api_url .. account, 
-				method = "DELETE",
-				headers = {["Content-Length"]=#obj.id},
-				source = ltn12.source.string(obj.id),
-				sink = ltn12.sink.table(tab)
-			}
-		end
-	end
-end)
 
 local pat = ("'%s: ' {[^ ]*} ' ' {[^ ]*} ' ' {.*}"):format(config.irc.nick)
 
@@ -65,8 +20,8 @@ function target(message)
 end
 
 commands = {
-	push = true,
-	pop = true
+	push = "push",
+	pop = "pop"
 }
 
 local qb = irc.new { nick = config.irc.nick }
@@ -84,11 +39,13 @@ qb:hook("OnChat", function(user, channel, message)
 	end
 
 	if commands[command] then
-		local id_ = tostring(id)
-		id = id + 1
-		queued_requests[id_] = channel
-		send_request(command, nick, item, id_)
-		qb:sendChat(channel, ("done [%s]"):format(id_))
+		local suc, res = api[commands[command]](nick, item)
+		if suc then
+			qb:sendChat(channel, "Success: " .. res)
+			return
+		else
+			qb:sendChat(channel, "Error: " .. res.msg)
+		end
 	end
 end)
 
@@ -100,15 +57,5 @@ end
 
 while true do
 	qb:think()
-	for k,v in pairs(queued_requests) do
-		local val = linda:get(k)
-		if val == false then
-			queued_requests[k] = nil
-			qb:sendChat(v, ("%s failed!"):format(k))
-		elseif type(val) == "string" then
-			queued_requests[k] = nil
-			qb:sendChat(v, val)
-		end
-	end
-	sleep(0.3)
+	sleep(0.05)
 end
